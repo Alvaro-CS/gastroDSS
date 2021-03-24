311089b72c0e2eb014397661f233203b378a382b
susan <susan@10.60.59.8>
2021-03-24 18:35:25 +0100

    welcome window

---
+++ src/gastrodss/WelcomeController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gastrodss;
+
+
+import java.awt.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class WelcomeController {
+
+    @FXML
+    private TextField patientname;
+
+    @FXML
+    void openSymptoms1(ActionEvent event) {
+
+    }
+
+}
+
